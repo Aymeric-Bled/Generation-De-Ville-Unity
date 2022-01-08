44b401d58febe8f082a99385c76a8b393df1c473
vleroy005 <58265517+vleroy005@users.noreply.github.com>
2022-01-08 12:39:50 +0100

    nav mesh tests (not working)

--- Assets/Scripts/Trafic.cs
+++ Assets/Scripts/Trafic.cs
@@ -20,16 +20,17 @@ public class Trafic : MonoBehaviour
 
         
         //GameObject test_ = Instantiate(test, new Vector3(-5, (float)-0, -5), Quaternion.Euler(0, 0, 90));
-        c = (NavMeshAgent)Instantiate(car, new Vector3(-5, 0, -5), Quaternion.Euler(0,0,0));
-        
+        c = (NavMeshAgent)Instantiate(car, new Vector3((float)3.5,(float)0.5,(float)37.5), Quaternion.Euler(0,0,0));
+        c.destination = new Vector3(-(float)18, (float)0.5, (float)37.5);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         if (c.velocity == new Vector3(0,0,0))
-        {   
-            c.destination = new Vector3(Random.Range(-15, 5), 0, Random.Range(-15, 5));
+        {
+            //c.destination = new Vector3(-18, (float)0.5, (float)37.5);
         }
         
     }
