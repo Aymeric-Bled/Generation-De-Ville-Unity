af9cc23400115c303756ead068daa443192b5140
Aymeric Bled <abled@enseirb-matmeca.fr>
2022-01-09 20:01:08 +0100

    NavMeshAgent

--- Assets/Scripts/StartRoads.cs
+++ Assets/Scripts/StartRoads.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
 using System;
 using Delaunay;
 using Delaunay.Geo;
@@ -13,6 +15,9 @@ public class StartRoads : MonoBehaviour
     public GameObject majorRoad;
     public GameObject batiment;
     public GameObject intersection;
+    public NavMeshAgent car;
+    public NavMeshSurface plane;
+
     public List<Tuple<float,LineSegment>> roads = new List<Tuple<float, LineSegment>>();
     public List<Tuple<float, LineSegment>> majorRoads = new List<Tuple<float, LineSegment>>();
     public List<Tuple<float, LineSegment>> waterSegments = new List<Tuple<float, LineSegment>>();
@@ -28,7 +33,7 @@ public class StartRoads : MonoBehaviour
     void Start()
     {
         readFileRoads(@".\Python\roads1.txt", @".\Python\water.txt");
-        Voronoi(300, 0.03f);
+        Voronoi(300, 0.1f);
         splitRoads();
         removeRoadsOnWater();
         foreach (Tuple<float, LineSegment> segment in majorRoads)
@@ -39,12 +44,33 @@ public class StartRoads : MonoBehaviour
         foreach (Tuple<float, LineSegment> segment in roads)
         {
             DrawRoad(segment, smallRoad, true);
-            addBuilding(segment, 0.5f, true, 0.1f, 0.1f, 0.1f);
-            addBuilding(segment, 0.5f, false, 0.1f, 0.1f, 0.1f);
+            addBuilding(segment, 0.5f, true, 0.05f, 0.05f, 0.1f);
+            addBuilding(segment, 0.5f, false, 0.05f, 0.05f, 0.1f);
+
         }
         foreach (Tuple<float, LineSegment> segment in waterSegments)
         {
             DrawRoad(segment, water, true);
+        }
+
+        plane = GetComponent<NavMeshSurface>();
+        plane.BuildNavMesh();
+        foreach (Tuple<float, LineSegment> segment in majorRoads)
+        {
+            LineSegment s = segment.Item2;
+            Vector2 left = (Vector2)s.p0;
+            Vector2 right = (Vector2)s.p1;
+            NavMeshHit closestHit;
+            Vector3 sourcePosition = new Vector3(((-left.y - right.y) * 0.5f * 0.05f + 5) * 10f, 0f, ((-left.x - right.x) * 0.5f * 0.05f + 5) * 10f);
+            //Vector3 sourcePosition = new Vector3(1f, 0f, 1f);
+            if (NavMesh.SamplePosition(sourcePosition, out closestHit, 500, 1))
+            {
+                NavMeshAgent c = (NavMeshAgent)Instantiate(car, closestHit.position, Quaternion.Euler(0, 0, 0));
+
+                NavMeshHit closestHit2;
+                NavMesh.SamplePosition(new Vector3(0f, 0f, 0f), out closestHit2, 500, 1);
+                c.destination = closestHit2.position;
+            }
         }
     }
 
@@ -56,7 +82,7 @@ public class StartRoads : MonoBehaviour
             string[] points = line.Split('\t');
             Vector2 left = new Vector2(float.Parse(points[0]), float.Parse(points[1]));
             Vector2 right = new Vector2(float.Parse(points[2]), float.Parse(points[3]));
-            majorRoads.Add(new Tuple<float, LineSegment>(0.1f, new LineSegment(left, right)));
+            majorRoads.Add(new Tuple<float, LineSegment>(0.2f, new LineSegment(left, right)));
         }
         lines = System.IO.File.ReadAllLines(waterFile);
         foreach (string line in lines)
@@ -75,22 +101,31 @@ public class StartRoads : MonoBehaviour
         LineSegment segment = route.Item2;
         Vector2 left = (Vector2)segment.p0;
         Vector2 right = (Vector2)segment.p1;
-        Vector3 vector = new Vector3((-left.y) * 0.05f + 5, 0, (-left.x) * 0.05f + 5);
+        Vector3 vector = new Vector3(((-left.y) * 0.05f + 5 )* 10f, 0, ((-left.x) * 0.05f + 5 )* 10f);
 
         GameObject road = Instantiate(gobject, vector, Quaternion.identity);
         if (gobject == water) {
-            road.transform.localScale = new Vector3(Mathf.Sqrt(Mathf.Pow(right.x - left.x, 2) + Mathf.Pow(right.y - left.y, 2)) * 0.05f, 0.01f, width);
+            road.transform.localScale = new Vector3(Mathf.Sqrt(Mathf.Pow(right.x - left.x, 2) + Mathf.Pow(right.y - left.y, 2)) * 0.05f * 10f, 0.01f * 10f, width * 10f);
         }
         else
         {
-            road.transform.localScale = new Vector3(Mathf.Sqrt(Mathf.Pow(right.x - left.x, 2) + Mathf.Pow(right.y - left.y, 2)) * 0.05f, 0.03f, width);
+            road.transform.localScale = new Vector3(Mathf.Sqrt(Mathf.Pow(right.x - left.x, 2) + Mathf.Pow(right.y - left.y, 2)) * 0.05f * 10f, 0.03f * 10f, width * 10f);
         }
 
         float theta = (left.y <= right.y) ? 180f - Mathf.Atan(((float)right.x - (float)left.x) / ((float)right.y - (float)left.y)) / Mathf.PI * 180f : -Mathf.Atan(((float)right.x - (float)left.x) / ((float)right.y - (float)left.y)) / Mathf.PI * 180f;
 
         road.transform.rotation = Quaternion.AngleAxis(theta, Vector3.up);
+
+        /*
+        if (gobject != water)
+        {
+            NavMeshSurface surface = road.GetComponent<NavMeshSurface>();
+            surface.BuildNavMesh();
+        }
+        */
+
         if (doTranslation)
-            road.transform.Translate(Mathf.Sqrt(Mathf.Pow(right.x - left.x, 2) + Mathf.Pow(right.y - left.y, 2)) * 0.5f * 0.05f, 0, 0);
+            road.transform.Translate(Mathf.Sqrt(Mathf.Pow(right.x - left.x, 2) + Mathf.Pow(right.y - left.y, 2)) * 0.5f * 0.05f * 10f, 0, 0);
         
     }
 
@@ -100,12 +135,12 @@ public class StartRoads : MonoBehaviour
         LineSegment segment = route.Item2;
         Vector2 left = (Vector2)segment.p0;
         Vector2 right = (Vector2)segment.p1;
-        Vector3 vector = new Vector3((-left.y) * 0.05f + 5, 0, (-left.x) * 0.05f + 5);
+        Vector3 vector = new Vector3(((-left.y) * 0.05f + 5) * 10f, 0, ((-left.x) * 0.05f + 5) * 10f);
         GameObject intersection1 = Instantiate(gobject, vector, Quaternion.identity);
-        intersection1.transform.localScale = new Vector3(width, 0.015f, width);
-        vector = new Vector3((-right.y) * 0.05f + 5, 0, (-right.x) * 0.05f + 5);
+        intersection1.transform.localScale = new Vector3(width * 10f, 0.015f * 10f, width * 10f);
+        vector = new Vector3(((-right.y) * 0.05f + 5) * 10f, 0, ((-right.x) * 0.05f + 5) * 10f);
         GameObject intersection2 = Instantiate(gobject, vector, Quaternion.identity);
-        intersection2.transform.localScale = new Vector3(width, 0.015f, width);
+        intersection2.transform.localScale = new Vector3(width * 10f, 0.015f * 10f, width * 10f);
     }
 
 
@@ -178,13 +213,15 @@ public class StartRoads : MonoBehaviour
         right.x = left.x + depth * norm.x;
         right.y = left.y + depth * norm.y;
 
-        GameObject building = Instantiate(batiment, new Vector3(-y *0.05f + 5,0.5f * height,-x * 0.05f + 5), Quaternion.identity);
-        building.transform.localScale = new Vector3(depth, height, width);
+        GameObject building = Instantiate(batiment, new Vector3((-y *0.05f + 5) * 10f, 0.5f * height * 10f, (-x * 0.05f + 5) * 10f), Quaternion.identity);
+        building.transform.localScale = new Vector3(depth * 10f, height * 10f, width * 10f);
 
         float theta = (left.y <= right.y) ? 180f - Mathf.Atan(((float)right.x - (float)left.x) / ((float)right.y - (float)left.y)) / Mathf.PI * 180f : -Mathf.Atan(((float)right.x - (float)left.x) / ((float)right.y - (float)left.y)) / Mathf.PI * 180f;
 
         building.transform.rotation = Quaternion.AngleAxis(theta, Vector3.up);
-        building.transform.Translate(Mathf.Sqrt(Mathf.Pow(right.x - left.x, 2) + Mathf.Pow(right.y - left.y, 2)) * 0.5f * 0.05f, 0, 0);
+        building.transform.Translate(Mathf.Sqrt(Mathf.Pow(right.x - left.x, 2) + Mathf.Pow(right.y - left.y, 2)) * 0.5f * 0.05f * 10f, 0, 0);
+
+        //building.GetComponent<Image>().sprite = Resources.Load<Sprite>();
 
         //Vector2 position = left + ratio * (right - left) + norm * roadWidth / 2;
     }
@@ -239,9 +276,9 @@ public class StartRoads : MonoBehaviour
                 Vector2 e2 = new Vector2(end2.x, end2.y);
                 Vector2 middle = new Vector2(x, a1 * x + b1);
 
-                Debug.Log("BEGIN: " + bg1.x + " " + bg1.y);
-                Debug.Log("MIDDLE: " + middle.x + " " + middle.y);
-                Debug.Log("END: " + e1.x + " " + e1.y);
+                //Debug.Log("BEGIN: " + bg1.x + " " + bg1.y);
+                //Debug.Log("MIDDLE: " + middle.x + " " + middle.y);
+                //Debug.Log("END: " + e1.x + " " + e1.y);
                 majorRoads.Add(new Tuple<float, LineSegment>(r1.Item1, new LineSegment(begin1, middle)));
                 majorRoads.Add(new Tuple<float, LineSegment>(r1.Item1, new LineSegment(middle, e1)));
                 roads.Add(new Tuple<float, LineSegment>(r2.Item1, new LineSegment(bg2, middle)));
